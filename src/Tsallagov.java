c57039c3916829769282edcb72fe89c455c46b54
Beknazar Suranchiyev <Suranchiyev96@gmail.com>
2020-08-02 13:28:27 -0400

    20 times

--- src/Tsallagov.java
+++ src/Tsallagov.java
@@ -2,10 +2,11 @@ public class Tsallagov{
     public static void main(String[] args) {
         System.out.println("Tsallagov");
         System.out.println("Alexander");
+
     }
 
     public static void print10() {
-    	for(int i = 0; i < 10; i++) {
+    	for(int i = 0; i < 20; i++) {
     		System.out.println("Hello World!");
     	}
     }
