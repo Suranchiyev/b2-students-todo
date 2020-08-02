3b18900578434f621cf9f85cb40b4c953a91f46a
Beknazar Suranchiyev <Suranchiyev96@gmail.com>
2020-08-02 13:24:36 -0400

    10 times HelloWorld

--- src/Tsallagov.java
+++ src/Tsallagov.java
@@ -2,4 +2,10 @@ public class Tsallagov{
     public static void main(String[] args) {
         System.out.println("Tsallagov");
     }
+
+    public static void print10() {
+    	for(int i = 0; i < 10; i++) {
+    		System.out.println("Hello World!");
+    	}
+    }
 }
